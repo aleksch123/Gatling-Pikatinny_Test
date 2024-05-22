567018c2d37557874953760745e0a1f27a6450f3
AleksCh <ssss.utest@gmail.com>
2024-05-22 14:55:47 +0300

    ref

--- src/test/java/ru/dot/invest/scenario/Invest.java
+++ src/test/java/ru/dot/invest/scenario/Invest.java
@@ -4,16 +4,16 @@ package ru.dot.invest.scenario;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import org.galaxio.gatling.transactions.Predef;
 import ru.dot.invest.pageobject.CreateOperation;
-import ru.domrf.dominvest.pageobject.Login;
 import ru.dot.invest.base.BaseTest;
 
 import static io.gatling.javaapi.core.CoreDsl.exec;
 
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static org.galaxio.gatling.transactions.Predef.*;
 
 
-public class Invest extends Predef.SimulationWithTransactions {
+public class Invest extends SimulationWithTransactions {
 
     private static final ScenarioBuilder defaultTest =
             scenario("Default Load Test")
